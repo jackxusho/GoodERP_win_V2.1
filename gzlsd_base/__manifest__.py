a24325850e49239f6ddabe6981367b5aed501bfd
AndrewLee <6658753@qq.com>
2018-06-26 23:46:43 +0800

    add    1.完成了基础的楼栋，房型，楼层的设置功能

---
+++ gzlsd_base/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GZLSD 基础设置管理",
+    'author': "广州菱致计算机科技有限公司",
+    'website': "www.sglsd.com",
+    'category': 'gzlsd',
+    'summary': '通用房间管理模块',
+    "description":
+    '''
+    该模块实现通用的房间管理，房型管理，房价管理，房间状态管理，未来房态查询，以及外部调用接口。。
+    ''',
+    'version': '0.1',
+    'depends': ["mail"],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'license': 'AGPL-3',
+}
